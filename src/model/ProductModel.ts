3417c7eebd4ae69a2cc9474de903b36ee6758c23
fernandosenacruz <fernandosenacruz@gmail.com>
2022-05-24 16:32:35 -0300

    Update ProductModel.ts and router.ts

--- src/model/ProductModel.ts
+++ src/model/ProductModel.ts
@@ -7,7 +7,7 @@ export default class ProductModel {
   public createProduct = async (
     code: string,
     name: string,
-    price: number,
+    price: number
   ): Promise<IProduct> => {
     const newProduct = await prisma.product.create({
       data: {
@@ -18,11 +18,51 @@ export default class ProductModel {
     });
 
     return newProduct;
-  }
+  };
 
   public getProducts = async (): Promise<IProduct[]> => {
     const products = await prisma.product.findMany();
 
     return products;
-  }
+  };
+
+  public getProduct = async (id: number): Promise<IProduct | null> => {
+    const product = await prisma.product.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    return product;
+  };
+
+  public updateProduct = async (
+    id: number,
+    code: string,
+    name: string,
+    price: number
+  ): Promise<IProduct | null> => {
+    const product = await prisma.product.update({
+      where: {
+        id,
+      },
+      data: {
+        code,
+        name,
+        price,
+      },
+    });
+
+    return product;
+  };
+
+  public deleteProduct = async (id: number): Promise<IProduct | null> => {
+    const product = await prisma.product.delete({
+      where: {
+        id,
+      },
+    });
+
+    return product;
+  };
 }
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import ProductController from './controller/ProductController';
+import FeedstockController from './controller/FeedstockController';
 
 const product = new ProductController();
+const feedstock = new FeedstockController();
 
 const router = Router();
 
+// Product routes
 router.post('/product', product.createProduct);
 
 router.get('/products', product.getProducts);
@@ -13,5 +16,16 @@ router.get('/product/:id', product.getProduct);
 router.put('/product/:id', product.updateProduct);
 
 router.delete('/product/:id', product.deleteProduct);
+
+// Feedstock routes
+router.post('/feedstock', feedstock.createFeedstock);
+
+router.get('/feedstocks', feedstock.getFeedstocks);
+
+router.get('/feedstock/:id', feedstock.getFeedstock);
+
+router.put('/feedstock/:id', feedstock.updateFeedstock);
+
+router.delete('/feedstock/:id', feedstock.deleteFeedstock);
 
 export default router;
